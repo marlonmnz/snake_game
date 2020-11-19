71b47db21ff198f54c5bebd0a59f95aab3eef922
Marlon Menezes <marlon_menezes@outlook.com>
2020-11-18 21:23:22 -0300

    Definir final do jogo

--- js/script.js
+++ js/script.js
@@ -50,11 +50,19 @@ function drawFood (){
 
 let direction = "right";
 
-function iniciarJogo(){
+function iniciarJogo(){    
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over! :(');
+        }
+    }
     
 
     criarBG();
@@ -74,9 +82,7 @@ function iniciarJogo(){
     }else{
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
-    }
-
-    
+    }    
 
     let newHead = {
         x: snakeX,
